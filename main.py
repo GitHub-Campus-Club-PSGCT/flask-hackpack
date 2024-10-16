18975a17d006c0f9d1a79770f37463383b0fd598
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-10-16 19:27:38 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello from GHCC!' # feel free to change this string
+
+# add new routes from here
+
+# stop adding new routes here
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
